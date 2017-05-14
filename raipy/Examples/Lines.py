8007c8856e0a2e323d7c2c165ebd78c80887164b
Yuki Arai <threemeninaboat3247@gmail.com>
2017-05-15 01:09:01 +0900

    amended Help.py

--- raipy/Examples/Lines.py
+++ raipy/Examples/Lines.py
@@ -13,10 +13,10 @@ NUM=7
 LINES_U=[['Line_'+str(i),'V'] for i in range(NUM)]
 LINES=['Line_'+str(i) for i in range(NUM)]
 COLORS=[QColor(255,0,0),QColor(255,255,0),QColor(128,255,0),QColor(0,255,255),QColor(0,128,255),QColor(255,0,128),QColor(255,0,255)]
-BOOLS=[['Stop_'+str(i),False] for i in range(NUM)]
+BOOLS=[['Noise_'+str(i),False] for i in range(NUM)]
 SLIDERS=[['Amplitude_'+str(i),0,100,50] for i in range(NUM)]
 DIALS=[['Phase_'+str(i),0,359,i] for i in range(NUM)]
-FLOATS=[['Base_'+str(i),i] for i in range(NUM)]
+FLOATS=[['Offset_'+str(i),i] for i in range(NUM)]
 
 class programThread(PyQt5.QtCore.QThread):
     outputSignal=PyQt5.QtCore.pyqtSignal(dict)   #You must emit all graph data at onece
